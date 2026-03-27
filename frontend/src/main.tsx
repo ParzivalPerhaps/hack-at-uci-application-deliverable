5d129bb5c9e7880f652487d42082f57fd95b3b56
Hayden Karp <113485583+ParzivalPerhaps@users.noreply.github.com>
2026-03-26 19:56:22 -0700

    initial commit: removed the python backend (no hate) in exchange for typescript and reinitialized  the frontend with tailwind and typescript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <div className="w-screen h-screen bg-[#111111]">
+      <App />
+    </div>
+  </React.StrictMode>,
 );
